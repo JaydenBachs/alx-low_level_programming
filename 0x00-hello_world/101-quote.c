aa399505f860261a919026ea4e9043de9960a061
JaydenBachs <korextoffymix@gmail.com>
2023-07-17 09:04:53 -0700

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A C program that prints a line to the standard error
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (0);
+}
